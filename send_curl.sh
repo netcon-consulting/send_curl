4247a964ae522022f8fdec472551214c080b8c19
Uwe Sommer <uwe@usommer.de>
2020-05-13 18:29:35 +0200

    final version

--- send_curl.sh
+++ send_curl.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
+############################################################################################################
 # Uwe Sommer 05/2020
 # Version 0.6
 # send email with curl directly (via TLS)
-# Some test messages included
+# Some test messages included (compose_message)
 # no need for additonal mailserver (just "dig" is required for dns queries)
 #
 # Features:
-#
-# - sending predefined message templates
-# - no need for MTA
-# - use SSL
+# - sending predefined message templates from shell
+# - no need for an MTA
+# - use SSL/TLS
 # - Bulk Sending of massmails for stresstests
 # - predefined message templates for content filter tests
 # - direkt server addressing without MX Lookup possible
 # - recipients can be specified directly or via listfile
 # - message templates are stored in separate file (compose_mail)
 #
-# Usage: 
-#  "source sm.sh" or include this in your bashrc as function
+# Install: 
+#  "source sm.sh" and "source  compose_message" or include these in your bashrc as function
+# Usage:
 #  "sm recipient type" 
 # Type can be: macro,html,url,virus,spam,corrupt,spoof or massmailing (1-100)
 # recipient can also be a file with a list of recipients (not for massmailings)
@@ -45,26 +46,22 @@ sm () {
 	# The sender Address may be changed
 	sender=evil@evil-domain.com
 	direct_server="$3"
-	# change this path to your git directory
-	template_path=~/Nextcloud/central-configs/send_curl
-	#template_path="$({BASH_SOURCE%/*}"
-	# template mail file will be created dynamically
+	# template mail file will be created dynamically by compose_message
 	mail=/tmp/email.txt
 	# get my external ip, do reverse lookup and use this as heloname
-	helo=$(dig +short -x $(dig +short myip.opendns.com @resolver1.opendns.com)|sed s/.$//)
+	helo=$(dig +short -x "$(dig +short myip.opendns.com @resolver1.opendns.com)"|sed s/.$//)
 	# catch missing params
-	if [ -z "$1" ]
-	then
+	if [ -z "$1" ] ;then
 		e_error "email address or email address file argument required"
 		return 1
-	elif [[ "$1" != *@* && ! -f "$1" ]]
-	then
+	elif [[ "$1" != *@* && ! -f "$1" ]] ;then
 		e_error "valid E-mail address required"
 		return 1
 	fi
 #############################################################################################################
 # functions
 #############################################################################################################
+	# find destination mailserver
 	dig_server () {
 		# if there is a third param, take this as server
 		if [ -n "$direct_server" ] ; then
@@ -75,40 +72,9 @@ sm () {
 		server="$switch"
 		# all other cases: dig for mx
 		else
-		server=$(dig +short +nodnssec mx $(echo "$each"| awk -F"@" '{print $2}') |sort -nr |tail -1 |awk '{print $2}'|sed s/.$//)
+		server=$(dig +short +nodnssec mx "$(echo "$each"| awk -F"@" '{print $2}')" |sort -nr |tail -1 |awk '{print $2}'|sed s/.$//)
 		fi
 	}
-	# more message templates in separate file
-	if [ -f $template_path/compose_message ] ; then 
-	   source $template_path/compose_message
-	else
-	# or use this base template
-	compose_message(){
-		case "$switch" in
-		# default spoof message
-		*)
-		(   
-			echo "X-SPAM: NO"
-			echo "Subject: Gruesse aus Berlin"
-			echo "To: <$each>"
-			echo "From: 'Angela Merkel' <bundeskanzler@bund.de>"
-			echo "Reply-to: <evil@evil-domain.com>"
-			echo "Message-Id: <$(date "+%m%d%H%M%Y")-114054033-4711@$helo>"
-			echo "Date: $(date -R)"
-			echo "MIME-Version: 1.0"
-			echo "Content-Type: text/plain; charset=utf-8; format=flowed"
-			echo "Content-Transfer-Encoding: 8bit"
-			echo "Content-Language: en-US"
-			echo ''
-			echo "Hallo $each"
-			echo ""
-			echo 'Liebe Gruesse aus Berlin'
-			echo 'Deine Angela'
-		) > "$mail"
-		;;
-		esac
-	}
-	fi
 	# sending mail template via curl to destination server
 	send_mail () {
 		curl -k --ssl smtp://"$server"/"$helo" --mail-from "$sender" --mail-rcpt "$each" --upload-file "$mail"
@@ -134,7 +100,6 @@ sm () {
 ############################################################################################################
 # enumerate listfile for recipients
 if [ -f "$recipients" ] ;then
-    # maybe: while read $recipients; do ..
     while read -r each   
 		do 
 		compose_message
@@ -144,13 +109,12 @@ if [ -f "$recipients" ] ;then
 	# first param is recipient
 	each="$1"
 	# detect numbers for massmailing (second param)
-	if [[ "$switch" =~ ^[0-9]+$ ]]; then
+	if [[ "$switch" =~ ^[0-9]+$ ]] ;then
 		echo "\033[31mMassmailing\033[0m"
 		dig_server
 		echo -e "Recipient:  \033[31m$each\033[0m"
 		echo -e "Mailserver: \033[31m$server\033[0m"
 		COUNTER=1
-		# do i need "i" ?
 		for i in $(seq "$switch"); do
 			echo "Message Number: $i"
      		compose_message
@@ -159,10 +123,11 @@ if [ -f "$recipients" ] ;then
 		done
 		echo "\033[31m$((COUNTER-1)) Messages were sent\033[0m"
 		else
-	# choose message body
+	# choose message body from external function
 	compose_message
 	# send email to destination server(s)
 	main
 	fi
 fi
 }
+############################################################################################################
